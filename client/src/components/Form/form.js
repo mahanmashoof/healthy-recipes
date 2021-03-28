46503fdbd3bdc2c3e18149b1d39b7824950902d4
Mahan Mashoof <mahan.mashoof@gmail.com>
2021-03-28 14:28:58 -0300

    form initiated

--- client/src/components/Form/form.js
+++ client/src/components/Form/form.js
@@ -1,11 +1,103 @@
-import React from 'react';
-import useStyles from './styles'
+import React, { useState } from "react";
+import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
 
 function Form() {
-    const classes = useStyles();
-    return(
-        <h1>Form</h1>
-    );
+  const [recipeData, setRecipeData] = useState({
+    creator: "",
+    title: "",
+    description: "",
+    ingredients: "",
+    selectedFile: "",
+  });
+
+  const classes = useStyles();
+
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Recipe</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={recipeData.creator}
+          onChange={(e) =>
+            setRecipeData({ ...recipeData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={recipeData.title}
+          onChange={(e) =>
+            setRecipeData({ ...recipeData, title: e.target.value })
+          }
+        />
+        <TextField
+          name="ingredients"
+          variant="outlined"
+          label="Ingredients"
+          fullWidth
+          value={recipeData.ingredients}
+          onChange={(e) =>
+            setRecipeData({ ...recipeData, ingredients: e.target.value })
+          }
+        />
+        <TextField
+          name="description"
+          variant="outlined"
+          label="Description"
+          fullWidth
+          value={recipeData.description}
+          onChange={(e) =>
+            setRecipeData({ ...recipeData, description: e.target.value })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setRecipeData({ ...recipeData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 }
 
 export default Form;
