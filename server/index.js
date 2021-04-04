3fdbecb5cb4b969a39bf76b497d48b965b6441e1
Mahan Mashoof <mahan.mashoof@gmail.com>
2021-04-04 10:20:58 -0300

    .env created

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import recipeRoutes from './routes/recipes.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
@@ -13,9 +15,8 @@ app.use(cors());
 app.use('/recipes', recipeRoutes); //defaults /recipes for methods
 
 //connect mongoose
-const CONNECTION_URL = 'mongodb+srv://mahan31:mahan31@cluster0.rmop3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) //promise function
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}) //promise function
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))) //if success
     .catch((error) => console.log(error.message)) //if error
 mongoose.set('useFindAndModify', false); //just to skip warnings
