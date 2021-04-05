875afe22d5b760f2bce95e39cddb431176f83eca
Mahan Mashoof <mahan.mashoof@gmail.com>
2021-04-05 19:38:27 -0300

    auth route layout prepared, separated from home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,22 @@
-import React, {useEffect, useState} from 'react';
-import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
-import recipes from './components/images/recipes.jpg'
-import Recipes from './components/Recipes/recipes';
-import Form from './components/Form/form';
-import useStyles from './styles'
-import {useDispatch} from 'react-redux';
-//import actions to dispatch them:
-import {getRecipes} from './actions/recipes';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getRecipes());
-    }, [currentId, dispatch]);
-
-    return(
-        <Container>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' align='center' >Healthy Recipes</Typography>
-                <img className={classes.image} src={recipes} alt='recipes' height='60' />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3} >
-                        <Grid item xs={12} sm={7}>
-                            <Recipes setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Switch>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/auth' exact component={Auth}></Route>
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 export default App;
