5bdfb6e9e0b7d15fe491bc6084822ae478178e38
Mahan Mashoof <mahan.mashoof@gmail.com>
2021-05-04 19:44:31 -0300

    like, delete and update user-dependent

--- client/src/components/Recipes/Recipe/recipe.js
+++ client/src/components/Recipes/Recipe/recipe.js
@@ -9,15 +9,48 @@ import {
   Typography,
 } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import ThumbUpAltOutlined from "@material-ui/icons/ThumbUpAltOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
-import {useDispatch} from 'react-redux';
-import {deleteRecipe, likeRecipe} from '../../../actions/recipes'
+import { useDispatch } from "react-redux";
+import { deleteRecipe, likeRecipe } from "../../../actions/recipes";
 
 function Recipe({ recipe, setCurrentId }) {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  const Likes = () => {
+    if (recipe.likes.length > 0) {
+      return recipe.likes.find(
+        (like) => like === (user?.result?.googleId || user?.result?._id)
+      ) ? (
+        <>
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp;
+          {recipe.likes.length > 2
+            ? `You and ${recipe.likes.length - 1} others`
+            : `${recipe.likes.length} like${
+                recipe.likes.length > 1 ? "s" : ""
+              }`}
+        </>
+      ) : (
+        <>
+          <ThumbUpAltOutlined fontSize="small" />
+          &nbsp;{recipe.likes.length}{" "}
+          {recipe.likes.length === 1 ? "Like" : "Likes"}
+        </>
+      );
+    }
+
+    return (
+      <>
+        <ThumbUpAltOutlined fontSize="small" />
+        &nbsp;Like
+      </>
+    );
+  };
 
   return (
     <Card className={classes.card}>
@@ -32,34 +65,51 @@ function Recipe({ recipe, setCurrentId }) {
           {moment(recipe.createdAt).fromNow()}
         </Typography>
       </div>
-      <div className={classes.overlay2}>
-        <Button style={{ color: "white" }} size="small" onClick={() => setCurrentId(recipe._id)}>
-          <MoreHorizIcon fontSize="default" />
-        </Button>
-      </div>
+      {(user?.result?.googleId === recipe?.creator ||
+        user?.result?._id === recipe?.creator) && (
+        <div className={classes.overlay2}>
+          <Button
+            style={{ color: "white" }}
+            size="small"
+            onClick={() => setCurrentId(recipe._id)}
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+      )}
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">
           {recipe.ingredients.map((ingredient) => `-${ingredient} `)}
         </Typography>
       </div>
       <Typography className={classes.title} variant="h5" gutterBottom>
-          {recipe.title}
-        </Typography>
+        {recipe.title}
+      </Typography>
       <CardContent>
-        <Typography variant="body2" color='textSecondary' component='p'>
+        <Typography variant="body2" color="textSecondary" component="p">
           {recipe.description}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-          <Button size='small' color='primary' onClick={() => dispatch(likeRecipe(recipe._id))}>
-              <ThumbUpAltIcon fontSize='small' />
-              &nbsp; Like &nbsp;
-              {recipe.likeCount}
+        <Button
+          size="small"
+          color="primary"
+          disabled={!user?.result}
+          onClick={() => dispatch(likeRecipe(recipe._id))}
+        >
+          <Likes />
+        </Button>
+        {(user?.result?.googleId === recipe?.creator ||
+          user?.result?._id === recipe?.creator) && (
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => dispatch(deleteRecipe(recipe._id))}
+          >
+            <DeleteIcon fontSize="small" />
+            &nbsp; Delete &nbsp;
           </Button>
-          <Button size='small' color='primary' onClick={() => dispatch(deleteRecipe(recipe._id))}>
-              <DeleteIcon fontSize='small' />
-              &nbsp; Delete &nbsp;
-          </Button>
+        )}
       </CardActions>
     </Card>
   );
